5a23bc5a7355eb4f292cee5f16dcd0d578c609e1
Johannes Donath <johannesd@torchmind.com>
2018-02-15 02:58:21 +0100

    Added a full module-info specification.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides a method of parsing and matching address ranges defined within the CIDR notation.
+ *
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+module com.torchmind.utility.cidr {
+  exports com.torchmind.utility.cidr;
+
+  requires static com.github.spotbugs.annotations;
+}
